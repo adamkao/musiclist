808af7e68a7f483c05c7b93b3ddfcca972660db9
Adam Kao <adamkao@gmail.com>
2020-08-16 15:59:13 -0700

    46

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const LocalStrategy = require('passport-local').Strategy;
 const index = require('./routes/index');
 const api = require('./routes/api/index');
 const users = require('./routes/api/users');
+const authentication = require('./routes/api/authentication');
 
 const app = express();
 // Connect Mongoose
@@ -43,20 +44,23 @@ app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Webpack Server
-const webpackCompiler = webpack(webpackConfig);
-app.use(webpackDevMiddleware(webpackCompiler, {
-  publicPath: webpackConfig.output.publicPath,
-  stats: {
-    colors: true,
-    chunks: true,
-    'errors-only': true,
-  },
-}));
-app.use(webpackHotMiddleware(webpackCompiler, {
-  log: console.log,
-}));
+if (process.env.NODE_ENV !== 'production') {
+  const webpackCompiler = webpack(webpackConfig);
+  app.use(webpackDevMiddleware(webpackCompiler, {
+    publicPath: webpackConfig.output.publicPath,
+    stats: {
+      colors: true,
+      chunks: true,
+      'errors-only': true,
+    },
+  }));
+  app.use(webpackHotMiddleware(webpackCompiler, {
+    log: console.log,
+  }));
+}
 app.use('/api', api);
 app.use('/api/users', users);
+app.use('/api/authentication', authentication);
 app.use('/*', index);
 
 // Configure Passport
--- models/user.js
+++ models/user.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
+
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const User = new Schema({
   username: String,
   firstName: String,
   lastName: String,
+  email: String,
 });
 
 User.plugin(passportLocalMongoose);
